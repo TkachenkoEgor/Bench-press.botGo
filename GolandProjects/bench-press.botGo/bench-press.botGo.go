a1db28a6ccbb083236f55f26c8d677a72c9ec72e
Loktar16 <Egorh16@mail.ru>
2022-03-05 10:29:24 +0300

    rework press bot

--- GolandProjects/bench-press.botGo/bench-press.botGo.go
+++ GolandProjects/bench-press.botGo/bench-press.botGo.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	"fmt"
+	_ "fmt"
 	"math"
-	_ "os"
+	"sort"
 )
 
 func main() {
 	var PM float64
 	fmt.Scan(&PM)
+	coef := make(map[int]float64)
+	var keys []int
+	coef[1] = 0.6
+	coef[2] = 0.7
+	coef[3] = 0.8
+	coef[4] = 0.9
 
-	var firstweek float64 = 0.6
-	var secondweek float64 = 0.7
-	var thirdweek float64 = 0.8
-	var fourthweek float64 = 0.9
+	sort.Ints(keys)
+	for _, k := range keys {
+		fmt.Println("Key:", k, "Value:", coef[k])
 
-	first := PM * firstweek
-	second := PM * secondweek
-	third := PM * thirdweek
-	fourth := PM * fourthweek
+	}
 
-	fmt.Println("1 и 2 неделя", (math.Floor(first*1000) / 1000), "кг 4 подхода 10 повторений")
-	fmt.Println("3 и 4 неделя", (math.Floor(second*1000) / 1000), "кг 5 подходов 6 повторений")
-	fmt.Println("5 и 6 неделя", (math.Floor(third*1000) / 1000), "кг 6 подходов 5 повторений")
-	fmt.Println("7 и 8 неделя", (math.Floor(fourth*1000) / 1000), "кг 3 подхода на 3 повторения")
+	for i, value := range coef {
+		PMNEW := value * PM
+		switch i {
+		case 0:
+			fmt.Println("начальный разовый максимум", PMNEW)
+
+		case 1:
+			fmt.Println("1 и 2 неделя", (math.Ceil(PMNEW)), "кг 4 подхода 10 повторений")
+		case 2:
+			fmt.Println("3 и 4 неделя", (math.Ceil(PMNEW)), "кг 5 подходов 6 повторений")
+		case 3:
+			fmt.Println("5 и 6 неделя", (math.Ceil(PMNEW)), "кг 6 подходов 5 повторений")
+		case 4:
+			fmt.Println("7 и 8 неделя", (math.Ceil(PMNEW)), "кг 3 подхода на 3 повторения")
+
+		}
+
+	}
+
 }
